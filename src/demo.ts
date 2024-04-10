176ce928582dcdbb0eb7bfcec883ce60b65b7fbe
Jeff Eaton <jeff@eaton.fyi>
2024-04-10 12:25:19 -0500

    Tidy up the demo

--- src/demo.ts
+++ src/demo.ts
@@ -5,12 +5,24 @@ await demo('./test/example.mbox');
 
 export async function demo(mailbox: string) {
   const parser = new MboxStreamer();
+  const addresses: Record<string, { sent: number, received: number }> = {};
 
-  parser.on('message', input => {
+  parser.on('message', (input, total) => {
     const m = formatMessage(input);
-    console.log(`${m.date?.toISOString().split('T')[0]}: "${m.subject}" from <${m.sender}>`);
+    addresses[m.sender ?? 'unknown'] ??= { sent: 0, received: 0 };
+    addresses[m.sender ?? 'unknown'].sent++;
+    addresses[m.recipient ?? 'unknown'] ??= { sent: 0, received: 0 };
+    addresses[m.recipient ?? 'unknown'].received++;
+
+    if (total % 100 === 0) {
+      console.log(total, 'messages processed...')
+    }
   });
-  parser.on('finish', total => console.log(`${total} messages processed.`));
+
+  parser.on('finish', total => {
+    console.log('Finished!', total, 'messages processed.');
+    console.log(addresses);
+  });
 
   console.log('Analyzing ./test/example.mbox...');
   return parser.parse(mailbox);
