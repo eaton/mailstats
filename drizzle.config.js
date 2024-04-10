1b9b50f988edee9ea007a3a3fb3ae2cab75d81a0
Jeff Eaton <jeff@eaton.fyi>
2024-04-10 10:52:36 -0500

    Keeping drizzle config in shape

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,12 +1,17 @@
 import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit'
+import jetpack from '@eatonfyi/fs-jetpack';
+
+// A little fancy footwork to ensure we respect output directories etc.
+let dbUrl = process.env.DB_URL;
+if (dbUrl !== ':memory:') {
+  dbUrl = jetpack.dir(process.env.OUTPUT_DIR ?? 'output').path(dbUrl ?? 'mailstats.db');
+}
 
 export default defineConfig({
- schema: "./src/crud/schema.ts",
+ schema: "./src/sqlite/schema.ts",
   driver: 'better-sqlite',
-  dbCredentials: {
-    url: process.env.DB_URL ?? ':memory:'
-  },
+  dbCredentials: { url: dbUrl },
   verbose: true,
   strict: true,
   out: './migrations',
