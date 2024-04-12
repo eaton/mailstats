960b4117f22c4aee61e58df4452dbd5ce97aabb7
Jeff Eaton <jeff@eaton.fyi>
2024-04-12 16:56:33 -0500

    Fixing an overlooked promise-return that choked on bad file writes

--- src/util/save-elements.ts
+++ src/util/save-elements.ts
@@ -14,24 +14,18 @@ export async function saveText(input: MboxMessage, dir: string): Promise<void> {
 
 export async function saveHtml(input: MboxMessage, dir: string): Promise<void> {
   if (!input.html) return Promise.resolve();
-
   const filename = jetpack.dir(dir).dir('html').path(`${input.mid}.html`);
-  return jetpack.writeAsync(filename, input.html).then(() => {
-    if (input.date) utimes(filename, input.date, input.date);
-    return void 0;
-  });
+  return jetpack.writeAsync(filename, input.html)
+    .then(() => { if (input.date) utimes(filename, input.date, input.date) });
 }
 
 // TODO: Consider just saving the raw headers rather than JSON-encoded ones.
 
 export async function saveHeaders(input: MboxMessage, output: string): Promise<void> {
   if (!input.headers) return Promise.resolve();
-
   const filename = jetpack.dir(output).dir('headers').path(`${input.mid}.json`);
-  return jetpack.writeAsync(filename, JSON.stringify(input.headers)).then(() => {
-    if (input.date) utimes(filename, input.date, input.date);
-    return void 0;
-  });
+  return jetpack.writeAsync(filename, JSON.stringify(input.headers))
+    .then(() => { if (input.date) utimes(filename, input.date, input.date) });
 }
 
 // TODO: Accept the full message instead of the attachment array, and set file mod dates to the message's date.
@@ -44,11 +38,9 @@ export async function saveAttachments(input: MboxMessage, dir: string) {
   for (const attachment of input.attachments) {
     const filename = attachment.filename.replaceAll('../', '/'); // What the fuck, people, seriously
     promises.push(
-      attachments.writeAsync(filename, attachment.content).then(() => {
-      if (input.date) utimes(attachments.path(filename), input.date, input.date);
-        return void 0;
-      })
+      attachments.writeAsync(filename, attachment.content)
+        .then(() => { if (input.date) utimes(attachments.path(filename), input.date, input.date) })
     );
   }
-  return promises;
+  return Promise.all(promises);
 }
